219297e8c4b5e3e2f97cbb0461071a1f7a05bafe
千々岩 / Chijiiwa <shingo.pad@gmail.com>
2022-12-05 08:18:53 +0000

    revert unuse socket

--- app.py
+++ app.py
@@ -7,33 +7,37 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 from revChatGPT.revChatGPT import Chatbot
 
 ChatGPTConfig = {
-        "Authorization": "<Your Bearer Token Here>", # This is optional
-        "session_token": os.environ.get('CHATGPT_SESSION_TOKEN')
-    }
+    "Authorization": "<Your Bearer Token Here>",  # This is optional
+    "session_token": os.environ.get('CHATGPT_SESSION_TOKEN')
+}
 
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"),
+          signing_secret=os.environ.get("SLACK_SIGNING_SECRET"))
 chatbot = Chatbot(ChatGPTConfig, conversation_id=None)
 
 # Listen for an event from the Events API
+
+
 @app.event("app_mention")
 def event_test(event, say):
     prompt = re.sub('(?:\s)<@[^, ]*|(?:^)<@[^, ]*', '', event['text'])
     response = chatbot.get_chat_response(prompt)
     user = event['user']
     user = f"<@{user}> が発言します:"
-    asked = ['>',prompt]
+    asked = ['>', prompt]
     asked = "".join(asked)
-    send = [user,asked,response["message"]]
+    send = [user, asked, response["message"]]
     send = "\n".join(send)
     say(send)
+
 
 def chatgpt_refresh():
     while True:
         chatbot.refresh_session()
         time.sleep(60)
 
+
 if __name__ == "__main__":
     thread = Thread(target=chatgpt_refresh)
     thread.start()
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
-    
+    app.start(port=int(os.environ.get("PORT", 3000)))
