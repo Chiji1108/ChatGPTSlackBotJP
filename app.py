e94f2fdd24a78440c35a4835b44cda03738ce6f6
千々岩 / Chijiiwa <shingo.pad@gmail.com>
2022-12-05 20:06:38 +0900

    update

--- app.py
+++ app.py
@@ -15,28 +15,23 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"),
           signing_secret=os.environ.get("SLACK_SIGNING_SECRET"))
 chatbot = Chatbot(ChatGPTConfig, conversation_id=None)
 
+@app.command("/hello-socket-mode")
+def hello_command(ack, body):
+    user_id = body["user_id"]
+    ack(f"Hi, <@{user_id}>!")
 # Listen for an event from the Events API
+
 @app.event("app_mention")
-def hoge(event, say):
-    print('hoge')
-    print(event['text'])
-    say("hoge")
-
-@app.event("message")
-def handle_message_events(body, logger):
-    logger.info(body)
-
-# @app.event("app_mention")
-# def event_test(event, say):
-#     prompt = re.sub('(?:\s)<@[^, ]*|(?:^)<@[^, ]*', '', event['text'])
-#     response = chatbot.get_chat_response(prompt)
-#     user = event['user']
-#     user = f"<@{user}> が発言します:"
-#     asked = ['>', prompt]
-#     asked = "".join(asked)
-#     send = [user, asked, response["message"]]
-#     send = "\n".join(send)
-#     say(send)
+def event_test(event, say):
+    prompt = re.sub('(?:\s)<@[^, ]*|(?:^)<@[^, ]*', '', event['text'])
+    response = chatbot.get_chat_response(prompt)
+    user = event['user']
+    user = f"<@{user}>"
+    asked = ['>', prompt]
+    asked = "".join(asked)
+    send = [user, asked, response["message"]]
+    send = "\n".join(send)
+    say(send)
 
 
 def chatgpt_refresh():
@@ -48,5 +43,4 @@ def chatgpt_refresh():
 if __name__ == "__main__":
     thread = Thread(target=chatgpt_refresh)
     thread.start()
-    print('hoge')
     app.start(port=int(os.environ.get("PORT", 8080)))
